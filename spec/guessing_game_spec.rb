ac1933e694546cd71ab60e3f517289775e28a4c4
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-09 14:27:01 +1200

    Rename specs according to powershop standards

--- spec/guessing_game_spec.rb
+++ spec/guessing_game_spec.rb
@@ -1,7 +1,7 @@
 require 'guessing_game'
 
-RSpec.describe GuessingGame, 'basics' do
-  it "should choose a number in a given range" do
+RSpec.describe GuessingGame, 'logic' do
+  it "chooses a number in a given range" do
     game = GuessingGame.new(1..100)
     expect(game.answer).to be >= 1
     expect(game.answer).to be <= 100
@@ -11,50 +11,50 @@ RSpec.describe GuessingGame, 'basics' do
     expect(game.answer).to be <= 300
   end
 
-  it "shouldn't change the answer" do
+  it "doesn't change the answer" do
     game = GuessingGame.new(1..100)
     answer1 = game.answer
     expect(game.answer).to eq(answer1)
   end
 
-  it "should allow a winning guess" do
+  it "allows a winning guess" do
     game = GuessingGame.new(1..1)
     expect(game.guess?(1)).to eq(true)
   end
 
-  it "should allow a losing guess" do
+  it "allows a losing guess" do
     game = GuessingGame.new(1..2)
     result1 = game.guess?(1)
     result2 = game.guess?(2)
     expect(result1).not_to eq(result2)
   end
 
-  it "should limit guesses by raising an error" do
+  it "limits guesses by raising an error" do
     game = GuessingGame.new(1..2)
     GuessingGame::MAX_GUESSES.times { game.guess?(1) }
     expect { game.guess?(2) }.to raise_error(GuessingGame::NoMoreGuesses)
   end
 
-  it "should reject guesses out of the correct range" do
+  it "rejects guesses out of the correct range" do
     game = GuessingGame.new(1..2)
     expect { game.guess?(3) }.to raise_error(GuessingGame::InvalidGuess)
   end
 
-  it "shouldn't count invalid guesses towards the guess limit" do
+  it "doesn't count invalid guesses towards the guess limit" do
     game = GuessingGame.new(1..2)
     (GuessingGame::MAX_GUESSES + 1).times do
       expect { game.guess?(3) }.to raise_error(GuessingGame::InvalidGuess)
     end
   end
 
-  it "should remember guesses" do
+  it "remembers guesses" do
     game = GuessingGame.new(1..2)
     game.guess?(1)
     game.guess?(2)
     expect(game.guesses).to eq([1, 2])
   end
 
-  it "shouldn't allow remembered guesses to be spoofed" do
+  it "allows remembered guesses to be spoofed" do
     game = GuessingGame.new(1..2)
     game.guess?(1)
     game.guess?(2)
